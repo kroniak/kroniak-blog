7204e316c31e4e39ced5ccf48d673157c4cfe99e
kroniak <me@kroniak.net>
2021-01-18 17:37:13 +0300

    add yandex metrica

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,6 +135,23 @@ module.exports = {
                 debug: false,
             },
         },
+        {
+            resolve: `gatsby-plugin-yandex-metrika`,
+            options: {
+                // The ID of yandex metrika.
+                trackingId: 71296738,
+                // Enabled a webvisor. The default value is `false`.
+                webvisor: true,
+                // Enables tracking a hash in URL. The default value is `false`.
+                trackHash: true,
+                // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
+                // and `true` means after the body (faster loading, less hits). The default value is `false`.
+                afterBody: true,
+                // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
+                // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+                defer: false,
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // This plugin is currently causing issues: https://github.com/gatsbyjs/gatsby/issues/25360
         //`gatsby-plugin-offline`,
